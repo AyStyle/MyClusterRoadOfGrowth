6dbe0e7cfda02d461a3ae951f7c173efd6ab046d
ankang <ankang@soyoung.com>
2020-10-30 23:06:22 +0800

    添加homework

---
+++ homework/src/main/java/ankang/cluster/homework/HomeworkApplication.java
@@ -0,0 +1,13 @@
+package ankang.cluster.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkApplication.class, args);
+	}
+
+}
